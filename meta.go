433e90b9ab4ecae00a9dfc2acf4594e3ac982534
sdttttt <sdttttt@163.com>
2021-04-05 17:58:07 +0800

    feat(huck): 🎉 Add from stat file load counter.

--- meta.go
+++ meta.go
@@ -5,3 +5,7 @@ const (
 
 	DEFAULT_COUNTER_FILE_NAME = "huck.counter.txt"
 )
+
+const (
+	STATISTICS_FILE_FORMAT_ERROR = "Huck has detected an existing statistics file, but the format is not correct. Please fix it manually."
+)
--- store.go
+++ store.go
@@ -1,9 +1,11 @@
 package huck
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -21,10 +23,47 @@ func InitCounterStorage() *CounterStorage {
 		lock:       new(sync.RWMutex),
 	}
 
+	store.LoadFromDisk(DEFAULT_COUNTER_FILE_NAME)
+
 	return store
 }
 
-func (store *CounterStorage) LoadFromDisk() {}
+func (store *CounterStorage) LoadFromDisk(filename string) {
+	if !IsFileExist(filename) {
+		return
+	}
+
+	b, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	content := BytesToString(b)
+	store.LoadFromString(content)
+}
+
+func (store *CounterStorage) LoadFromString(content string) {
+
+	counters := strings.Split(content, "\n")
+	for _, counter := range counters {
+		counterArr := strings.Split(counter, ":")
+
+		if len(counterArr) == 1 {
+			continue
+		} else if len(counterArr) != 2 {
+			log.Fatalln(STATISTICS_FILE_FORMAT_ERROR)
+		}
+
+		count, err := strconv.ParseUint(strings.TrimSpace(counterArr[1]), 10, 64)
+		if err != nil {
+			log.Println(STATISTICS_FILE_FORMAT_ERROR)
+			log.Fatalln(err.Error())
+		}
+
+		store.counterMap[strings.TrimSpace(counterArr[0])] = count
+	}
+}
 
 func (store *CounterStorage) Save(key string, value uint64) {
 	store.counterMap[key] = value
